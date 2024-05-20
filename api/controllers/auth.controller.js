0218151de700f77122f4ad14e83d3d48d50b3d3d
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-05-20 10:43:16 +0700

    create signup API

---
+++ api/controllers/auth.controller.js
@@ -0,0 +1,29 @@
+import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
+export const signup = async (req, res) => {
+  const { username, email, password } = req.body;
+
+  if (
+    !username ||
+    !email ||
+    !password ||
+    username === "" ||
+    password === "" ||
+    email === ""
+  ) {
+    return res.status(400).jon({ message: "All fields are required" });
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+  const newUser = new User({
+    username,
+    email,
+    password: hashedPassword,
+  });
+  try {
+    await newUser.save();
+    res.json("Signup Successfull");
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
 dotenv.config();
 mongoose
   .connect(process.env.MONGO)
@@ -13,9 +14,10 @@ mongoose
   });
 
 const app = express();
-
+app.use(express.json());
 app.listen(3000, () => {
   console.log("Sever is running on port 3000!");
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes );
