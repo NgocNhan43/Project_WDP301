77773519f5ef4055f7cc8600c1d75316122f6400
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-05-21 16:49:01 +0700

    Fix signIn page

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -43,8 +43,8 @@ export default function SignUp() {
           </form>
           <div className="flex gap-2 text-sm mt-5">
           <span>Have an account ?</span>
-          <Link to='/sign-up' className="text-blue-500">
-            Sign Up
+          <Link to='/sign-in' className="text-blue-500">
+            Sign In
           </Link>
           </div>
         </div>
