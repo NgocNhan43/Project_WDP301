a0a8c057811e775a6140131f9015cba6640f2b4f
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-05-26 11:15:23 +0700

    Create and add the footer component

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
-
+import jwt from "jsonwebtoken";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
@@ -20,7 +20,7 @@ export const signup = async (req, res, next) => {
     // Kiểm tra xem username hoặc email đã tồn tại chưa
     const existingUser = await User.findOne({ $or: [{ username }, { email }] });
     if (existingUser) {
-      return next(errorHandler(400, "Username orr email existing")); // Nếu tồn tại, trả về lỗi 400
+      return next(errorHandler(400, "Username or email existing")); // Nếu tồn tại, trả về lỗi 400
     }
     // Mã hóa mật khẩu sử dụng bcrypt với độ dài muối là 10
     const hashedPassword = await bcryptjs.hashSync(password, 10); //mã hóa password
@@ -35,6 +35,37 @@ export const signup = async (req, res, next) => {
     // Trả về phản hồi thành công
 
     res.json("Signup Successfull");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(400, "User not found"));
+    }
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(400, "Invalid passWord"));
+    }
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+
+    res
+      .status(200)
+      .cookie("access  _token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
   } catch (error) {
     next(error);
   }
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,6 +1,8 @@
 import express  from 'express';
 import  {signup}  from '../controllers/auth.controller.js';
+import  {signin}  from '../controllers/auth.controller.js';
 const router = express.Router();
 router.post('/signup', signup);
+router.post('/signin', signin);
 export default router;
 //Route đăng ký người dùng: Sử dụng hàm signup để xử lý các yêu cầu đăng ký.
