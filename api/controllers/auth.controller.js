f7d453739109c2aca24978bd0e476fb522322b63
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-05-26 09:36:04 +0700

    update auth thêm phần sử lý kiểm tra email tồn tại

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -15,14 +15,25 @@ export const signup = async (req, res, next) => {
   ) {
     next(errorHandler(400, "All fields are required"));
   }
-  const hashedPassword = await bcryptjs.hashSync(password, 10); //mã hóa password
-  const newUser = new User({
-    username,
-    email,
-    password: hashedPassword,
-  });
+
   try {
+    // Kiểm tra xem username hoặc email đã tồn tại chưa
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return next(errorHandler(400, "Username orr email existing")); // Nếu tồn tại, trả về lỗi 400
+    }
+    // Mã hóa mật khẩu sử dụng bcrypt với độ dài muối là 10
+    const hashedPassword = await bcryptjs.hashSync(password, 10); //mã hóa password
+    // Tạo một đối tượng người dùng mới với thông tin đã nhập và mật khẩu đã mã hóa
+    const newUser = new User({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    // Lưu người dùng mới vào cơ sở dữ liệu
     await newUser.save();
+    // Trả về phản hồi thành công
+
     res.json("Signup Successfull");
   } catch (error) {
     next(error);
