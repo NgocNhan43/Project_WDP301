23e253aebc75d6f941713ebc5799ff4b6b48ce77
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-05-29 08:08:18 +0700

    Fix lại thư mục chưa footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ import SignUp from './pages/SignUp'
 import Dashboard from './pages/Dashboard'
 import Projects from './pages/Projects'
 import Header from './components/Header'
-import Footer from './pages/Footer'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
