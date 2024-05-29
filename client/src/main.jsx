6ed2e515fc83dcd468f2cc4d060bc4f6691122f1
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-05-29 08:04:57 +0700

    Add redux persist (lưu thông tin người dùng sau khi đăng nhập)

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,14 @@
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { store, persistor } from "./redux/store.js";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { store } from './redux/store.js';
-import { Provider } from 'react-redux';
-ReactDOM.createRoot(document.getElementById('root')).render(
-
-
-  <Provider store={store}>
-    <App />
-  </Provider>,
-)
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <PersistGate persistor={persistor}>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </PersistGate>
+);
