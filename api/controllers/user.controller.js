ecefbc3023d4141f5a9a8e4580ad04be8cce4b6f
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-06-12 00:24:19 +0700

    comple API gets USER

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,12 +1,11 @@
-import bcryptjs from "bcryptjs";
+import bcryptjs from 'bcryptjs';
 import { errorHandler } from '../utils/error.js';
 import User from '../models/user.model.js';
-export const test =
-  ("/test",
-  (req, res) => {
-    res.json({ message: "API is working" });
-  });
-//update user account
+
+export const test = (req, res) => {
+  res.json({ message: 'API is working!' });
+};
+
 export const updateUser = async (req, res, next) => {
   if (req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to update this user'));
@@ -55,10 +54,8 @@ export const updateUser = async (req, res, next) => {
   }
 };
 
-//API delete route
-
 export const deleteUser = async (req, res, next) => {
-  if (req.user.id !== req.params.userId) {
+  if (!req.user.isAdmin && req.user.id !== req.params.userId) {
     return next(errorHandler(403, 'You are not allowed to delete this user'));
   }
   try {
@@ -75,6 +72,61 @@ export const signout = (req, res, next) => {
       .clearCookie('access_token')
       .status(200)
       .json('User has been signed out');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUsers = async (req, res, next) => {
+  if (!req.user.isAdmin) {
+    return next(errorHandler(403, 'You are not allowed to see all users'));
+  }
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.sort === 'asc' ? 1 : -1;
+
+    const users = await User.find()
+      .sort({ createdAt: sortDirection })
+      .skip(startIndex)
+      .limit(limit);
+
+    const usersWithoutPassword = users.map((user) => {
+      const { password, ...rest } = user._doc;
+      return rest;
+    });
+
+    const totalUsers = await User.countDocuments();
+
+    const now = new Date();
+
+    const oneMonthAgo = new Date(
+      now.getFullYear(),
+      now.getMonth() - 1,
+      now.getDate()
+    );
+    const lastMonthUsers = await User.countDocuments({
+      createdAt: { $gte: oneMonthAgo },
+    });
+
+    res.status(200).json({
+      users: usersWithoutPassword,
+      totalUsers,
+      lastMonthUsers,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const getUser = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.params.userId);
+    if (!user) {
+      return next(errorHandler(404, 'User not found'));
+    }
+    const { password, ...rest } = user._doc;
+    res.status(200).json(rest);
   } catch (error) {
     next(error);
   }
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,11 +1,21 @@
 import express from 'express';
-import  {updateUser,test, deleteUser,signout}  from '../controllers/user.controller.js';
+import {
+  deleteUser,
+  getUser,
+  getUsers,
+  signout,
+  test,
+  updateUser,
+} from '../controllers/user.controller.js';
 import { verifyToken } from '../utils/verifyUser.js';
 
-
 const router = express.Router();
+
 router.get('/test', test);
 router.put('/update/:userId', verifyToken, updateUser);
 router.delete('/delete/:userId', verifyToken, deleteUser);
 router.post('/signout', signout);
-  export default router;
+router.get('/getusers', verifyToken, getUsers);
+router.get('/:userId', getUser);
+
+export default router;
