f621a0a8f2afc5606d45db6c2784a0668bd3f42e
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-06-11 18:06:12 +0700

    add funtion admin

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -57,7 +57,14 @@ export const signin = async (req, res, next) => {
       return next(errorHandler(400, "Invalid passWord"));
     }
 
-    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    // const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+
+    const token = jwt.sign(
+      { id: validUser._id, isAdmin: validUser.isAdmin },
+      process.env.JWT_SECRET
+    );
+
+
     const { password: pass, ...rest } = validUser._doc;
 
     res
@@ -76,7 +83,14 @@ export const google = async (req, res, next) => {
   try {
     const user = await User.findOne({ email });
     if (user) {
-      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+      // const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+
+      const token = jwt.sign(
+        { id: user._id, isAdmin: user.isAdmin },
+        process.env.JWT_SECRET
+      );
+
+
       const { password, ...rest } = user._doc;
       res
         .status(200)
@@ -98,10 +112,7 @@ export const google = async (req, res, next) => {
         profilePicture: googlePhotoUrl,
       });
       await newUser.save();
-      const token = jwt.sign(
-        { id: newUser._id },
-        process.env.JWT_SECRET
-      );
+      const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET);
       const { password, ...rest } = newUser._doc;
       res
         .status(200)
